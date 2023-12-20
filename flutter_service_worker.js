529fece405b93b43f270620e1e061ff8c8101627
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2023-12-19 21:49:30 -0500

    Liba app + fix
    fix porque no servia bien

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,8 +28,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "57dd04b2072367fdf2e80fdf77330e2f",
-"/": "57dd04b2072367fdf2e80fdf77330e2f",
+"index.html": "64d14d989e9935437bcad34cf1cb7f6d",
+"/": "64d14d989e9935437bcad34cf1cb7f6d",
 "main.dart.js": "27fbecf3896553773d94e8532a5075b9",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
