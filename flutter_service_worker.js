4e7290ed7b52d5ff87ca29b1352f605fce2b7555
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2023-12-28 16:21:58 -0500

    New fix, v1.0.24
    fixes in version 1.0.24
    
    fixes de responsive
    fixes en streambuilder
    vamos avanznaod poco a poco

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3ae45adcaa5509e722f8f771eb48c1fc",
-"/": "3ae45adcaa5509e722f8f771eb48c1fc",
-"main.dart.js": "21dd85e60c6aee8eb76c1ef981121556",
+"index.html": "559f3fec9b502867bec4b00b5710892d",
+"/": "559f3fec9b502867bec4b00b5710892d",
+"main.dart.js": "7b84d4bf121482537011ba15a890fb9c",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
