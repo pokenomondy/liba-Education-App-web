7cfd91afdb36b7991333b1fb082018036e0b9049
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2023-12-29 01:24:34 -0500

    New fixes, detalles servicio
    Detalles servicio responsive y vista de archivo
    Aún no es final, pero funciona por el mometno

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544",
 "assets/AssetManifest.json": "92595af3477a4a752167f9dcb58d6463",
 "assets/FontManifest.json": "ce1d0ae664e14238869feff07c71ce21",
-"assets/fonts/MaterialIcons-Regular.otf": "a86edcf75d5593cb4dd4735cfd3df91d",
+"assets/fonts/MaterialIcons-Regular.otf": "8a5bac983ff1b9c1c929b1f647d532e6",
 "assets/NOTICES": "f4abeb6b2ef2ac0ec8c85534c77dda7b",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/packages/fluent_ui/assets/AcrylicNoise.png": "81f27726c45346351eca125bd062e9a7",
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "559f3fec9b502867bec4b00b5710892d",
-"/": "559f3fec9b502867bec4b00b5710892d",
-"main.dart.js": "7b84d4bf121482537011ba15a890fb9c",
+"index.html": "e2dcf913f3a3ac9a4c9ce07675735608",
+"/": "e2dcf913f3a3ac9a4c9ce07675735608",
+"main.dart.js": "1d328e44de6d08ead49a9dc0f6e11bbc",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
