338420fbdb0d9d90ed76bd0fd2a1655f924fc000
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2024-01-05 23:28:33 -0500

    Liba 1.26
    Aplicación casi funcioal vamos bien

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544",
 "assets/AssetManifest.json": "92595af3477a4a752167f9dcb58d6463",
 "assets/FontManifest.json": "ce1d0ae664e14238869feff07c71ce21",
-"assets/fonts/MaterialIcons-Regular.otf": "8a5bac983ff1b9c1c929b1f647d532e6",
+"assets/fonts/MaterialIcons-Regular.otf": "d45b827ed0498cbe804ed7b8311901fc",
 "assets/NOTICES": "f4abeb6b2ef2ac0ec8c85534c77dda7b",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/packages/fluent_ui/assets/AcrylicNoise.png": "81f27726c45346351eca125bd062e9a7",
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6083424f53f292dc2bb9823b9d682d18",
-"/": "6083424f53f292dc2bb9823b9d682d18",
-"main.dart.js": "639a2a9905de77c160328a601c1cf2c6",
+"index.html": "c6320e809c0021e18a6240b353a3643a",
+"/": "c6320e809c0021e18a6240b353a3643a",
+"main.dart.js": "5d96a225cb3acd6a488fefd8f3952cad",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
