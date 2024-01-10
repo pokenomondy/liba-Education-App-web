6bd79247e5a5ee5f8be9b50dc78c48f69384e3d8
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2024-01-10 18:03:11 -0500

    new fixed update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544",
 "assets/AssetManifest.json": "92595af3477a4a752167f9dcb58d6463",
 "assets/FontManifest.json": "ce1d0ae664e14238869feff07c71ce21",
-"assets/fonts/MaterialIcons-Regular.otf": "b811214a547effc689cc10ccd6578504",
+"assets/fonts/MaterialIcons-Regular.otf": "58c9717c2f11f3087e3e87fd509e572b",
 "assets/NOTICES": "f4abeb6b2ef2ac0ec8c85534c77dda7b",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/packages/fluent_ui/assets/AcrylicNoise.png": "81f27726c45346351eca125bd062e9a7",
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "63d03a4e2875da85050d070205c3dd12",
-"/": "63d03a4e2875da85050d070205c3dd12",
-"main.dart.js": "96f6dee3c97f6531b185397ade4bb9da",
+"index.html": "c0eaef8b2b5f0062a482c33cfa507793",
+"/": "c0eaef8b2b5f0062a482c33cfa507793",
+"main.dart.js": "56b5019ddc763e05738f51d1b986de23",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
