3c793e499dfc81271886620dac898930af13f9a5
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2024-01-27 09:36:06 -0500

    v fixes finales para video de demostración
    Link de entregas con cauchUrl
    Arreglos en entrar a la app

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f20b546690d8ec5bf145c92d047dea2b",
-"/": "f20b546690d8ec5bf145c92d047dea2b",
-"main.dart.js": "1f9b24a96de48791b84d5cfed21be4cf",
+"index.html": "e4dac2acd0b6304c8ac5216cb9273ac1",
+"/": "e4dac2acd0b6304c8ac5216cb9273ac1",
+"main.dart.js": "854c75335ec70289fa99d68f699831e9",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
