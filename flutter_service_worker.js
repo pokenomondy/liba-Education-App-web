82f45610478dff698861ce75a22c765a4fe2f55e
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2024-02-07 21:33:14 -0500

    version fix
    arreglo de apartado de sistema en agendar

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "5557fc52affa7e6787d59089cf542544
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "29a1c4cb37240c9ea9a959b741ab816b",
-"/": "29a1c4cb37240c9ea9a959b741ab816b",
-"main.dart.js": "84bbd1caa2827c71316aff74bc6e0a94",
+"index.html": "486c9177106f0a10af8ba798e3b8d3c2",
+"/": "486c9177106f0a10af8ba798e3b8d3c2",
+"main.dart.js": "1e108ccf88d6957527ee3af85d5aa419",
 "manifest.json": "5b9895d06a9e4a517790b35d60a9da8b",
 "version.json": "056f384927d8e2d09d8231af787b20ad"};
 // The application shell files that are downloaded before a service worker can
